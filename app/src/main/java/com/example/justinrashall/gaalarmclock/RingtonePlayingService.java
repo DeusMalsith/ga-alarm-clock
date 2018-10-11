b3fca27e915845a80e9367cd4b0239ff351e0961
DeusMalsith <justinrashallphotography@gmail.com>
2018-10-10 20:28:53 -0700

    Added forest.mp3 file and added a trigger for the mp3

--- app/src/main/java/com/example/justinrashall/gaalarmclock/RingtonePlayingService.java
+++ app/src/main/java/com/example/justinrashall/gaalarmclock/RingtonePlayingService.java
@@ -3,12 +3,15 @@ package com.example.justinrashall.gaalarmclock;
 import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.IBinder;
 import android.util.Log;
 import android.support.annotation.Nullable;
 import android.widget.Toast;
 
 public class RingtonePlayingService extends Service {
+
+    MediaPlayer media_song;
 
     @Nullable
     @Override
@@ -17,16 +20,12 @@ public class RingtonePlayingService extends Service {
     }
 
     @Override
-    public void onCreate() {
-        mNM = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
-
-        // Display a notification about us starting. We put an icon in the status bar.
-        showNotification();
-    }
-
-    @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.i("LocalService", "Received start id " + startId + ": " + intent);
+
+        media_song = MediaPlayer.create(this, R.raw.forest);
+        media_song.start();
+
         return START_NOT_STICKY;
     }
 
