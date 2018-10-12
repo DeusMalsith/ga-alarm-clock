db6cfce5595217be802592b16af1d3bc6f83acc9
DeusMalsith <justinrashallphotography@gmail.com>
2018-10-11 18:03:28 -0700

    Added if else statements for button pressed states and added notification for when alarm goes off

--- app/src/main/java/com/example/justinrashall/gaalarmclock/RingtonePlayingService.java
+++ app/src/main/java/com/example/justinrashall/gaalarmclock/RingtonePlayingService.java
@@ -1,6 +1,8 @@
 package com.example.justinrashall.gaalarmclock;
 
+import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
@@ -13,6 +15,7 @@ public class RingtonePlayingService extends Service {
 
     MediaPlayer media_song;
     int startId;
+    boolean isRunning;
 
     @Nullable
     @Override
@@ -29,6 +32,28 @@ public class RingtonePlayingService extends Service {
 
         Log.e("Ringtone state: extra is ", state);
 
+        // notification
+        // set up the notification service
+        NotificationManager notify_manager = (NotificationManager)
+                getSystemService(NOTIFICATION_SERVICE);
+
+        // set up an intent that goes to the Main Activity
+        Intent intent_main_activity = new Intent(this.getApplicationContext(), MainActivity.class);
+
+        // set up a pending intent for "setContentIntent"
+        PendingIntent pending_intent_main_activity = PendingIntent.getActivity(this, 0, intent_main_activity, 0);
+
+        // make the notification parameters
+        Notification notification_popup = new Notification.Builder(this)
+                .setContentTitle("An alarm is going off!")
+                .setContentText("Click me!")
+                .setContentIntent(pending_intent_main_activity)
+                .setAutoCancel(true)
+                .build();
+
+
+        // this converts the extra strings from the intent
+        // to start IDs, values 0 or 1
         assert state != null;
         switch (state) {
             case "alarm on":
@@ -42,9 +67,59 @@ public class RingtonePlayingService extends Service {
                 break;
         }
 
+        // if else statements
 
-        media_song = MediaPlayer.create(this, R.raw.forest);
-        media_song.start();
+        // if there is no music playing and the user pressed "alarm on"
+        // music should start playing
+        if (!this.isRunning && startId == 1) {
+            Log.e("there is no music", "and you want start");
+
+            // create an instance of the media player
+            media_song = MediaPlayer.create(this, R.raw.forest);
+            media_song.start();
+
+            this.isRunning = true;
+            this.startId = 0;
+        }
+        // if ther is music paying and the user pressed "alarm off"
+        // music should stop playing
+        else if (this.isRunning && startId == 0) {
+            Log.e("there is music", "and you want end");
+
+            media_song.stop();
+            media_song.reset();
+
+            this.isRunning = false;
+            this.startId = 0;
+        }
+        // if the user presses random buttons. Primarily for bug proofing.
+
+        // if there is no music playing and the user pressed "alarm off"
+        // do nothing
+        else if (!this.isRunning && startId == 0) {
+            Log.e("there is no music", "and you want end");
+
+            this.isRunning = false;
+            this.startId = 0;
+
+        }
+        // if there is music playing and the user pressed "alarm on"
+        // do nothing
+        else if (this.isRunning && startId == 1) {
+            Log.e("there is music", "and you want start");
+
+            this.isRunning = true;
+            this.startId = 1;
+
+        }
+        // odd event catcher
+        else {
+            Log.e("else", "odd event produced");
+        }
+
+
+//        media_song = MediaPlayer.create(this, R.raw.forest);
+//        media_song.start();
 
 
 
@@ -54,6 +129,9 @@ public class RingtonePlayingService extends Service {
     @Override
     public void onDestroy() {
         // Tell the user we stopped
-        Toast.makeText(this, "on Destroy called", Toast.LENGTH_SHORT).show();
+        Log.e("on Destroy called", "DESTROYED!");
+
+        super.onDestroy();
+        this.isRunning = false;
     }
 }
