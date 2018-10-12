3c39fb24b80e7f1e1c331054eb82f9bbccca8cf9
DeusMalsith <justinrashallphotography@gmail.com>
2018-10-11 20:11:31 -0700

    Added readme

--- app/src/main/java/com/example/justinrashall/gaalarmclock/RingtonePlayingService.java
+++ app/src/main/java/com/example/justinrashall/gaalarmclock/RingtonePlayingService.java
@@ -1,18 +1,17 @@
 package com.example.justinrashall.gaalarmclock;
 
-import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.IBinder;
-import android.util.Log;
 import android.support.annotation.Nullable;
-import android.widget.Toast;
+import android.util.Log;
 
 public class RingtonePlayingService extends Service {
 
     MediaPlayer media_song;
     int startId;
+    boolean isRunning;
 
     @Nullable
     @Override
@@ -29,6 +28,8 @@ public class RingtonePlayingService extends Service {
 
         Log.e("Ringtone state: extra is ", state);
 
+        // this converts the extra strings from the intent
+        // to start IDs, values 0 or 1
         assert state != null;
         switch (state) {
             case "alarm on":
@@ -42,11 +43,55 @@ public class RingtonePlayingService extends Service {
                 break;
         }
 
+        // if else statements
 
-        media_song = MediaPlayer.create(this, R.raw.forest);
-        media_song.start();
+        // if there is no music playing and the user pressed "alarm on"
+        // music should start playing
+        if (!this.isRunning && startId == 1) {
+            Log.e("there is no music", "and you want start");
 
+            // create an instance of the media player
+            media_song = MediaPlayer.create(this, R.raw.forest);
+            media_song.start();
 
+            this.isRunning = true;
+            this.startId = 0;
+        }
+        // if ther is music paying and the user pressed "alarm off"
+        // music should stop playing
+        else if (this.isRunning && startId == 0) {
+            Log.e("there is music", "and you want end");
+
+            media_song.stop();
+            media_song.reset();
+
+            this.isRunning = false;
+            this.startId = 0;
+        }
+        // if the user presses random buttons. Primarily for bug proofing.
+
+        // if there is no music playing and the user pressed "alarm off"
+        // do nothing
+        else if (!this.isRunning && startId == 0) {
+            Log.e("there is no music", "and you want end");
+
+            this.isRunning = false;
+            this.startId = 0;
+
+        }
+        // if there is music playing and the user pressed "alarm on"
+        // do nothing
+        else if (this.isRunning && startId == 1) {
+            Log.e("there is music", "and you want start");
+
+            this.isRunning = true;
+            this.startId = 1;
+
+        }
+        // odd event catcher
+        else {
+            Log.e("else", "odd event produced");
+        }
 
         return START_NOT_STICKY;
     }
@@ -54,6 +99,9 @@ public class RingtonePlayingService extends Service {
     @Override
     public void onDestroy() {
         // Tell the user we stopped
-        Toast.makeText(this, "on Destroy called", Toast.LENGTH_SHORT).show();
+        Log.e("on Destroy called", "DESTROYED!");
+
+        super.onDestroy();
+        this.isRunning = false;
     }
 }
